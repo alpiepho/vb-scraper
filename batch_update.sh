7e360529ca0a7395cfe4772c3004d283c3c2600a
apiepho <alpiepho@gmail.com>
2023-03-11 08:45:16 -0700

    add states

--- batch_update.sh
+++ batch_update.sh
@@ -2,10 +2,12 @@
 # # go run main.go -configfile=config_all.json
 # go run main.go -configfile config_spokane300.json
 # go run main.go -configfile config_spokaneStates.json
-go run main.go -configfile=config_spokaneCoaches.json
+# go run main.go -configfile=config_spokaneCoaches.json
 # go run main.go -configfile=config_chicago300.json
 # go run main.go -configfile=config_chicago100.json
 # go run main.go -configfile=config_reno300.json
+go run main.go -configfile=config_arizona.json
+go run main.go -configfile=config_colorado.json
 # go run main.go -configfile=config_florida.json
 # # go run main.go -configfile=config_kip.json
 # # go run main.go -configfile=config_kip2.json
