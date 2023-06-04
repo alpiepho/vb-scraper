be43c9055fab44a3c739bd82c352703707d59dd6
apiepho <alpiepho@gmail.com>
2023-06-04 13:32:41 -0600

    update

--- batch_update.sh
+++ batch_update.sh
@@ -1,22 +1,14 @@
 # # go run main.go -configfile=config.json
 # # go run main.go -configfile=config_all.json
-# go run main.go -configfile config_spokane300.json
-# go run main.go -configfile config_spokaneStates.json
 # go run main.go -configfile=config_spokaneCoaches.json
-# go run main.go -configfile=config_chicago300.json
-# go run main.go -configfile=config_chicago100.json
 # go run main.go -configfile=config_chicagoCoaches.json
 # go run main.go -configfile=config_reno300.json
+
+# following have virtual tours
 # go run main.go -configfile=config_arizona.json
 # go run main.go -configfile=config_colorado.json
 # go run main.go -configfile=config_florida.json
-# # go run main.go -configfile=config_kip.json
-# # go run main.go -configfile=config_kip2.json
-# # go run main.go -configfile=config_kipSpokane.json
-# # go run main.go -configfile=config_kipCamp.json
-# go run main.go -configfile=config_16.json
-# go run main.go -configfile=config_16_emails.json
-# go run main.go -configfile=config_kipCamp23.json
+# go run main.go -configfile=config_texas.json
 go run main.go -configfile=config_aauCoaches.json
 # go run main.go -configfile=config_kplist051223.json
 # go run main.go -configfile=config_coachesPollDII.json
